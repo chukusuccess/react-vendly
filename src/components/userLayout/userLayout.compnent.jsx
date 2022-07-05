786d2baf1cd87d6ca69f96faeb031d14981919ba
chukusuccess <successchuku5@gmail.com>
2022-07-05 17:55:51 +0100

    first commit

---
+++ src/components/userLayout/userLayout.compnent.jsx
@@ -0,0 +1,40 @@
+import { Layout } from "antd";
+import React from "react";
+const { Header, Footer, Sider, Content } = Layout;
+
+export const UserLayout = () => (
+  <>
+    <Layout>
+      <Header>Header</Header>
+      <Content>Content</Content>
+      <Footer>Footer</Footer>
+    </Layout>
+
+    <Layout>
+      <Header>Header</Header>
+      <Layout>
+        <Sider>Sider</Sider>
+        <Content>Content</Content>
+      </Layout>
+      <Footer>Footer</Footer>
+    </Layout>
+
+    <Layout>
+      <Header>Header</Header>
+      <Layout>
+        <Content>Content</Content>
+        <Sider>Sider</Sider>
+      </Layout>
+      <Footer>Footer</Footer>
+    </Layout>
+
+    <Layout>
+      <Sider>Sider</Sider>
+      <Layout>
+        <Header>Header</Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </Layout>
+  </>
+);
