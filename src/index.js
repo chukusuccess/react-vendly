8e0c7ed87d7a7f765b8592ea180595e59ca32f52
Precious-josiah-codes <45442741+Precious-josiah-codes@users.noreply.github.com>
2022-07-07 04:31:52 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
-import "antd/dist/antd.less";
-import "./index.less";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import ProductDetailsContextProvider from './context/ProductDetailsContext';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <ProductDetailsContextProvider>
       <App />
+    </ProductDetailsContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
